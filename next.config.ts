22b43c020127982e04a2797982015e8d743e5647
Medi stock <medistock.us@gmail.com>
2025-10-24 07:05:48 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,8 @@ const nextConfig: NextConfig = {
   },
   devIndicators: {
     allowedDevOrigins: [
-        'https://6000-firebase-studio-1761131049061.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev'
+        'https://6000-firebase-studio-1761131049061.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev',
+        'https://9000-firebase-studio-1761131049061.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev'
     ]
   },
   webpack: (config) => {
